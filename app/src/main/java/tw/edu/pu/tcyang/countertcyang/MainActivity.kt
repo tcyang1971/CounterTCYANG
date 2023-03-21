fbfe63341ad290444f9f921b2911db4ea4b5e266
tcyang1971 <tcyang@gm.pu.edu.tw>
2023-03-21 17:05:55 +0800

    加入樂透功能

--- app/src/main/java/tw/edu/pu/tcyang/countertcyang/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/tcyang/countertcyang/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.tcyang.countertcyang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var txv: TextView = findViewById(R.id.txv)
         txv.text = "0"
+    }
+
+    fun happy(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = (1..100).random().toString()
     }
 }
