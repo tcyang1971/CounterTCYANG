fbf18d8c07cb02cca249412b45d841a5a66a39cc
s1101761 <s1101761@gm.pu.edu.tw>
2023-03-22 11:52:14 +0800

    螢幕觸控3

--- app/src/main/java/tw/edu/pu/tcyang/countertcyang/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/tcyang/countertcyang/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         var txv:TextView = findViewById(R.id.txv)
-        txv.text = "螢幕觸控中"
+        txv.text = "螢幕觸控"
         return true
     }
 }
